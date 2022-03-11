6af188f38148579a07ab20bed4cc1294e509469f
= <=>
2022-03-10 19:14:18 -0600

    App will accept get requests at /customers and numeric post requests at /customers. Added mux for routing.

--- app/app.go
+++ app/app.go
@@ -1,19 +1,36 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func Start() {
 
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 
 	// defining routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post Request Received")
+	
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
+
 
 }
